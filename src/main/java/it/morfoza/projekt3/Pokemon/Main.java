d56e180dcb426a242f9ed7853b82cc79b407c800
widzew <klop99@wp.pl>
2016-07-29 15:14:23 +0200

    Pokemon

---
+++ src/main/java/it/morfoza/projekt3/Pokemon/Main.java
@@ -0,0 +1,16 @@
+package it.morfoza.projekt3.Pokemon;
+
+/**
+ * Created by widzew on 2016-07-29.
+ */
+public class Main {
+
+    public static void  main(String[] args) {
+
+        Pokemon pokemon = new Pokemon();
+
+        String pokemonDescription = pokemon.toString();
+
+        System.out.println(pokemonDescription);
+    }
+}
---
+++ src/main/java/it/morfoza/projekt3/Pokemon/Pokemon.java
@@ -0,0 +1,7 @@
+package it.morfoza.projekt3.Pokemon;
+
+/**
+ * Created by widzew on 2016-07-29.
+ */
+public class Pokemon {
+}
