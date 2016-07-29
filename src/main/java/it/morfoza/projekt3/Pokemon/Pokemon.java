c6dcfbc412d7cbb7eefe22dc695c24970b7c6925
widzew <klop99@wp.pl>
2016-07-29 16:27:01 +0200

    Pokemon attack

--- src/main/java/it/morfoza/projekt3/Pokemon/Pokemon.java
+++ src/main/java/it/morfoza/projekt3/Pokemon/Pokemon.java
@@ -4,4 +4,31 @@ package it.morfoza.projekt3.Pokemon;
  * Created by widzew on 2016-07-29.
  */
 public class Pokemon {
+    private String name;
+    private long life = 1000;
+    private long combatPower;
+
+    public Pokemon(String name, long combatPower) {
+        this.name = name;
+        this.combatPower = combatPower;
+    }
+    public String toString() {
+        return "Pokemon " + name + " HP: " + life;
+    }
+
+    public void takeAttack(long attackPower) {
+        life = life - attackPower;
+    }
+
+    public boolean isAlive() {
+        if (life > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public void attack(Pokemon enemy) {
+        enemy.takeAttack(combatPower);
+    }
 }
